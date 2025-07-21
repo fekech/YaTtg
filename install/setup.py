6ab882e564b9fb54bd0a2356ca50a899930de9b4
fekech <filyakert@gmail.com>
2025-07-21 16:26:09 +0300

    setup init

---
+++ install/setup.py
@@ -0,0 +1,14 @@
+import os
+import configparser
+
+_pip = "pip install "
+_cfg = configparser.ConfigParser()
+
+os.system('Python -V')
+input ('Нажмите любую клавишу что бы продолжить...')
+
+_cfg.read(os.getcwd()+chr(92)+'install.ini')
+os.system("pip install --upgrade pip")
+os.system(_pip + _cfg.get('NAMES','aiogram'))
+
+input ('Нажмите любую клавишу что бы продолжить...')
